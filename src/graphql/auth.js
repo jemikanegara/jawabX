c3719d3117b150023a52b39088ebfd9bd7522ec4
jemikanegara <jemikanegara@gmail.com>
2019-09-14 21:49:29 +0800

    clean up commented old ajax call

--- src/graphql/auth.js
+++ src/graphql/auth.js
@@ -12,22 +12,4 @@ export const tokenCheck = async () => {
         auth.set(true);
     }
     return response;
-
-    // return await fetch(URL, {
-    //     method: JSON_OPT.method,
-    //     headers: { ...JSON_OPT.headers, 'Authorization': `Bearer ${token}` },
-    //     body: JSON.stringify({
-    //         query
-    //     })
-    // })
-    //     .then(r => r.json())
-    //     .then(res => {
-    //         let response;
-    //         if (res.data && res.data.auth) {
-    //             auth.set(true);
-    //             response = true;
-    //         }
-    //         return response;
-    //     })
-    //     .catch(err => console.log(err));
 }
