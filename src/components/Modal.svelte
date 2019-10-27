399ee64956560b8fae8f8fc9dec7b9ac1c543452
jemikanegara <jemikanegara@gmail.com>
2019-10-27 21:25:11 +0800

    account edit

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,45 +1,3 @@
-<script>
-  import { createEventDispatcher } from "svelte";
-
-  export let show;
-  export let value;
-  export let data;
-  export let label;
-  export let submitMessage;
-  export let edit;
-  export let passwordData;
-
-  const dispatch = createEventDispatcher();
-  let timer;
-  let passwordError;
-  let passwordErrorMessage;
-
-  //   let modalData = {
-  //     header: "",
-  //     error: "",
-  //     success: "",
-  //     submit: ""
-  //   };
-
-  $: disabled = passwordData.isError || data.isError ? true : false;
-
-  const keyup = () => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      dispatch("keyup");
-    }, 1000);
-  };
-
-  const submit = () => {
-    if (disabled) return;
-    dispatch("submit");
-  };
-
-  const close = () => {
-    dispatch("close");
-  };
-</script>
-
 <style>
   .modal-wrap {
     top: 0;
@@ -53,52 +11,34 @@
     background: rgba(0, 0, 0, 0.6);
     z-index: 99;
   }
-  .input {
-    width: 100%;
-    display: flex;
-    align-items: center;
-  }
-  .input > * {
-    max-width: 50%;
-    width: 50%;
-  }
-  .input:last-child {
-    margin-top: 10px;
-  }
   .modal-content {
     margin-bottom: 10px;
     white-space: pre;
   }
+  .ui.modal {
+    display: block !important;
+  }
 </style>
 
-<div class="modal-wrap" style={`display: ${show ? 'flex' : 'none'}`}>
-  <div class="ui modal" style={`display: ${show ? 'block' : 'none'}`}>
-    <div class="header">{edit ? 'Ganti ' : ''}{label}</div>
+<div class="modal-wrap">
+  <div class="ui modal">
+    <div class="header">
+      <!-- Header -->
+      <slot name="header" />
+    </div>
     <div class="content">
-      <div class="modal-content">{data.content}</div>
+      <div class="modal-content">
+        <!-- Content -->
+        <slot name="content" />
+      </div>
       <div class="inline field">
-        <div class="ui input">
-          <input type="text" bind:value on:keyup={keyup} />
-          {#if data.isError}
-            <div class="ui left pointing red basic label">{data.error}</div>
-          {:else if data.isError !== undefined}
-            <div class="ui left pointing green basic label">{data.success}</div>
-          {:else}
-            <div class="ui left pointing yellow basic label">
-              Masukkan {label}
-            </div>
-          {/if}
-        </div>
-        <slot />
+          <!-- Input -->
+          <slot name="input" />
       </div>
     </div>
     <div class="actions">
-      <div class="ui cancel red button" on:click={close}>Batal</div>
-      <div
-        class={`ui approve button primary ${disabled ? 'disabled' : ''}`}
-        on:click={submit}>
-        {submitMessage}
-      </div>
+      <!-- Buttons -->
+      <slot name="buttons" />
     </div>
   </div>
 </div>
--- src/routes/account/index.svelte
+++ src/routes/account/index.svelte
@@ -1,126 +1,83 @@
+<script context="module">
+  import { accountText } from "./_account.data.js";
+  export async function preload() {
+    return { accountText };
+  }
+</script>
+
 <script>
   import { onMount } from "svelte";
   import { ajax } from "../../graphql/settings.js";
   import { query as getQuery } from "../../graphql/queries/account.js";
-  import { query as updateQuery } from "../../graphql/mutations/update.js";
-  import { query as checkQuery } from "../../graphql/queries/check.js";
-  import Modal from "../../components/Modal.svelte";
+  import { auth } from "../../stores/auth.js";
+  import AccountEdit from "./_AccountEdit.svelte";
 
-  let account;
-  let password;
-  let accountKeys = {
-    name: {
-      label: "Nama",
-      edit: false
-    },
-    username: {
-      label: "Username",
-      edit: false,
-      data: {
-        header: "Tambah Username",
-        content:
-          " Username merupakan nama unik yang dapat dimiliki pengguna. \n Pemilihan username hanya dapat dilakukan sekali, selanjutnya tidak dapat diubah",
-        error: "Username sudah terdaftar atau tidak dapat digunakan",
-        success: "Username dapat digunakan",
-        submit: "Simpan Username",
-        isError: undefined
-      }
-    },
-    email: {
-      label: "Email",
-      edit: false,
-      data: {
-        header: "Ubah Email",
-        content: "Ubah email",
-        error: "Email sudah terdaftar atau tidak dapat digunakan",
-        success: "Email dapat digunakan",
-        submit: "Simpan Email",
-        isError: undefined
-      }
-    },
-    phone: {
-      label: "Telepon",
-      edit: false,
-      data: {
-        header: "Ubah Nomor Telepon",
-        content: "Ubah nomor telepon",
-        error: "Nomor sudah terdaftar atau tidak dapat digunakan",
-        success: "Nomor dapat digunakan",
-        submit: "Simpan Nomor",
-        isError: undefined
-      }
+  export let accountText;
+  let account = {};
+  let token = "";
+
+  let accountEdit = {};
+
+  // Fetch Account Data
+  const fetchAccount = async () => {
+    const res = await ajax(fetch, { query: getQuery, token });
+    if (!res.data || res.data.errors) {
+      account = {};
+    } else {
+      const data = res.data.account;
+      account = data;
+      account.newPassword = "";
     }
   };
 
-  const passwordData = {
-    content: "Masukkan Password",
-    error: "Password minimal 6 karakter",
-    isError: false
-  };
-
-  let token = localStorage.getItem("token");
-  let showModal = false;
-  let showPasswordModal = false;
-
-  const initialize = async () => {
-    const res = await ajax(fetch, { query: getQuery, token });
-    account = !res.data || res.data.errors ? null : res.data.account;
-  };
-
-  const check = async (e, key) => {
-    let variables = {};
-    variables[key] = account[key];
-    const res = await ajax(fetch, { query: checkQuery, variables, token });
-    const isAvailable = res.data.accountCheck;
-    if (!isAvailable) accountKeys[key].data.isError = true;
-    else accountKeys[key].data.isError = false;
-  };
-
-  const modify = async (e, key) => {
-    let variables = {};
-    variables[key] = account[key];
-    variables.password = password;
-    const res = await ajax(fetch, { query: updateQuery, variables, token });
-    console.log(res);
-    if (res.data.update) close(e, key);
-    else console.log(res);
-  };
-
-  const edit = (e, key) => {
-    accountKeys[key].edit = true;
-    showModal = true;
-  };
-
-  const close = (e, key) => {
-    accountKeys[key].edit = false;
-    showModal = false;
-  };
-
-  let timer;
-
-  const validatePassword = e => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      if (password.length < 6) passwordData.isError = true;
-      else passwordData.isError = false;
-    }, 1000);
+  // Set Mode to Edit or View Account
+  const setMode = async (key, editMode) => {
+    let unsubscribe = auth.subscribe(function(isAuth) {
+      if (isAuth) accountText[key].edit = editMode;
+    });
+    unsubscribe();
   };
 
   onMount(() => {
-    initialize();
+    token = localStorage.getItem("token");
+    fetchAccount();
+    accountEdit = Object.assign({}, account);
   });
 </script>
 
 <style>
+  .profile {
+    display: flex;
+    flex-wrap: wrap;
+    flex: 1 0 auto;
+  }
+  .left {
+    padding: 5px 0;
+    margin-right: 30px;
+    margin-bottom: 10px;
+  }
+  .image {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    width: 150px;
+    height: 150px;
+    border: 2px solid #aaa;
+    color: #aaa;
+    font-size: 50px;
+    margin-bottom: 5px;
+    background: #eee;
+  }
+  .image i {
+    font-size: 80px;
+  }
   .data {
     padding: 5px 0;
     display: flex;
   }
   label {
     width: 100px;
-  }
-  button {
-    margin-top: 10px !important;
+    font-weight: 700;
   }
   .content {
     margin-right: 10px;
@@ -133,80 +90,52 @@
   .edit-link:hover {
     color: #ff5722;
   }
-  .password {
-    margin-top: 10px;
-  }
-  .password .input {
-    width: 100%;
-  }
-  .password input,
-  .password .ui.label {
-    width: 50%;
-    max-width: 50%;
-  }
-  .password .password-label {
-    margin-bottom: 5px;
-  }
 </style>
 
 <h2>Pengaturan</h2>
-<form>
-  {#if account}
+<div class="profile">
+  <div class="left">
+    <div class="image">
+      <i class="user icon" />
+    </div>
+    <button class="ui button primary fluid">Ubah Foto Profil</button>
+  </div>
+  <div class="right">
     {#each Object.keys(account) as key}
       <div class="data">
-        <label>{accountKeys[key].label}</label>
-        {#if accountKeys[key].edit}
-          <Modal
-            bind:value={account[key]}
-            {passwordData}
-            bind:show={showModal}
-            label={accountKeys[key].label}
-            bind:data={accountKeys[key].data}
-            on:submit={e => modify(e, key)}
-            on:keyup={e => check(e, key)}
-            on:close={e => close(e, key)}
-            submitMessage={'Simpan'}
-            edit={true}>
-            <div class="password">
-              <div class="password-label">Masukkan Password</div>
-              <div class="ui input">
-                <input
-                  type="password"
-                  bind:value={password}
-                  on:keyup={validatePassword}
-                  autocomplete="current-password" />
-                {#if passwordData.isError}
-                  <div class="ui left pointing red basic label">
-                    {passwordData.error}
-                  </div>
-                {/if}
-              </div>
-            </div>
-          </Modal>
-        {:else if account[key]}
-          <span class="content">{account[key]}</span>
-          {#if key !== 'name'}
+        {#if accountText[key]}
+          <label>{accountText[key].label}</label>
+          {#if accountText[key].edit}
+            <AccountEdit
+              on:close={() => {
+                setMode(key, false);
+              }}
+              bind:accountEdit
+              {key}
+              {token}
+              keyInfo={accountText[key]} />
+          {:else if account[key]}
+            <span class="content">{account[key]}</span>
+            {#if key !== 'name'}
+              <span
+                class="edit-link"
+                on:click={e => {
+                  setMode(key, true);
+                }}>
+                Edit
+              </span>
+            {/if}
+          {:else}
             <span
               class="edit-link"
               on:click={e => {
-                edit(e, key);
+                setMode(key, true);
               }}>
-              Edit
+              Tambah
             </span>
           {/if}
-        {:else}
-          <span
-            class="edit-link"
-            on:click={e => {
-              edit(e, key);
-            }}>
-            Tambah
-          </span>
         {/if}
       </div>
     {/each}
-  {/if}
-  <div>
-    <button class="ui button primary">Ubah</button>
   </div>
-</form>
+</div>
